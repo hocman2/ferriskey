ecc9ec464478f0d05c6d77eb5c02489f9b3e0014
Matthis <matthishoc@gmail.com>
2025-09-10 15:47:32 +0200

    split generation and formatting logic

--- core/src/infrastructure/recovery_code/mod.rs
+++ core/src/infrastructure/recovery_code/mod.rs
@@ -1,31 +1,24 @@
-use crate::domain::trident::ports::MfaRecoveryCodeRepository;
 use crate::domain::trident::entities::MfaRecoveryCode;
-use crate::infrastructure::repositories::random_bytes_recovery_code::RandBytesMfaRecoveryCodeRepository;
+use crate::domain::trident::ports::RecoveryCodeRepository;
+use crate::infrastructure::repositories::random_bytes_recovery_code::{
+    B32Split4RecoveryCodeFormatter, RandBytesRecoveryCodeRepository,
+};
 
 #[derive(Clone)]
-pub enum MfaRecoveryCodeRepoAny {
-    RandomBytes6(RandBytesMfaRecoveryCodeRepository::<6>),
-    RandomBytes8(RandBytesMfaRecoveryCodeRepository::<8>),
-    RandomBytes10(RandBytesMfaRecoveryCodeRepository::<10>),
-    RandomBytes12(RandBytesMfaRecoveryCodeRepository::<12>),
+pub enum RecoveryCodeRepoAny {
+    RandomBytes10(RandBytesRecoveryCodeRepository<10, B32Split4RecoveryCodeFormatter>),
 }
 
-impl MfaRecoveryCodeRepository for MfaRecoveryCodeRepoAny {
+impl RecoveryCodeRepository for RecoveryCodeRepoAny {
     fn generate_recovery_code(&self) -> MfaRecoveryCode {
         match self {
-            MfaRecoveryCodeRepoAny::RandomBytes6(repo)  => repo.generate_recovery_code(),
-            MfaRecoveryCodeRepoAny::RandomBytes8(repo)  => repo.generate_recovery_code(),
-            MfaRecoveryCodeRepoAny::RandomBytes10(repo) => repo.generate_recovery_code(),
-            MfaRecoveryCodeRepoAny::RandomBytes12(repo) => repo.generate_recovery_code(),
+            RecoveryCodeRepoAny::RandomBytes10(repo) => repo.generate_recovery_code(),
         }
     }
 
     fn to_string(&self, code: &MfaRecoveryCode) -> String {
         match self {
-            MfaRecoveryCodeRepoAny::RandomBytes6(repo)  => repo.to_string(&code),
-            MfaRecoveryCodeRepoAny::RandomBytes8(repo)  => repo.to_string(&code),
-            MfaRecoveryCodeRepoAny::RandomBytes10(repo) => repo.to_string(&code),
-            MfaRecoveryCodeRepoAny::RandomBytes12(repo) => repo.to_string(&code),
+            RecoveryCodeRepoAny::RandomBytes10(repo) => repo.to_string(&code),
         }
     }
 
