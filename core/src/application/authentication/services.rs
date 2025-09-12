0a4aedfe1eeb5c63ed92e3fe76c5f077e36bac68
Matthis <matthishoc@gmail.com>
2025-09-12 11:10:16 +0200

    changed jwt service in AuthenticateFactory to pub(crate) instead of private

--- core/src/application/authentication/services.rs
+++ core/src/application/authentication/services.rs
@@ -38,7 +38,7 @@ pub struct AuthenticateFactory {
     client_repository: ClientRepoAny,
     credential_repository: CredentialRepoAny,
     hasher_repository: HasherRepoAny,
-    jwt_service: DefaultJwtService,
+    pub(crate) jwt_service: DefaultJwtService,
 }
 
 impl AuthenticateFactory {
